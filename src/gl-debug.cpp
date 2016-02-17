ae82a61ca77ea6352e4fd3ea8d620d8dafae301f
Mathew Velasquez <mathewvq@gmail.com>
2016-02-17 17:37:39 -0500

    Fix build on Ubuntu

--- src/gl-debug.cpp
+++ src/gl-debug.cpp
@@ -70,7 +70,7 @@ static void APIENTRY arbDebugFunc(GLenum source,
 	(void) id;
 	(void) length;
 
-	if (severity == GL_DEBUG_SEVERITY_NOTIFICATION)
+	if (severity == 0x826B) // GL_DEBUG_SEVERITY_NOTIFICATION
 		return;
 
 	p->writeTimestamp();
