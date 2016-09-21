33d6f38f96c2f216dba05311693d14b3d2fea513
Michael Shirt <girakacheezer@gmail.com>
2016-09-20 20:53:20 -0600

    No one needs this bat file but me
    So I will just tell git to ignore it

--- rpgscript.bat
+++ rpgscript.bat
@@ -1,2 +1,2 @@
-rpgscript.rb "C:\Users\Michael\Documents\oneshot src\mkxp-oneshot\scripts" "C:\Users\Michael\Dropbox\Oneshot Project\Game"
+"C:\Ruby22-x64\bin\ruby.exe" rpgscript.rb "C:\Users\Michael\Documents\oneshot src\mkxp-oneshot\scripts" "C:\Users\Michael\Dropbox\Oneshot Project\Game"
 pause
