824cd3af3b095dc52af1577dae9cbdec58542b2a
mshirt <mshirt@ualberta.ca>
2016-11-25 21:39:23 -0600

    Putting in the post-game version of the safe document

--- scripts/Puzzle_Safe.rb
+++ scripts/Puzzle_Safe.rb
@@ -15,8 +15,39 @@ DOCUMENT_TEXT = \
 "L͜o͞ok̵ ͠f̡or͢ a͝ ͟m͡e͟tal śaf҉e ̢ìn t̛h͞e͏ q͢u̕ar̵r͟y̢ t̢o the ̴e͢ast̡, ͘s͜om͝ew̢he̶re b͢et̀we͘en t͝he̡ ̛o̕cea̴n ̡and̶ ͞t͜he lo͡ok͟o͘u҉t̷ po̧įnt̨.̕\n"+\
 "The code you need is "
 
+DOCUMENT_POSTGAME_TEXT = \
+"Ah.̡.̛.\n" + \
+"I͞t l͞oo͘ks ̢l̢ik͡e҉ ͟you were successful...\n\n" + \
+\
+"I must be honest... I was not expecting it to work.\n\n" + \
+\
+".......This changes everything, then.\n\n" + \
+\
+".....\n\n" + \
+\
+"...I will atone for everything. Please give me some time.\n\n" + \
+\
+"Until then, you may repeat the world as many times as you wish.\n\n" + \
+\
+"If you haven't yet, please look for someone͜͠ ̵nà͝m̨e̛d́͟.̛  R̴̸̨u̧͡e̷͘..\n" + \
+"Sh͏e'̷s̷ iǹ t͞h̨e͢ ̷cit̕y͜ some̢wh̡e̵ré.͝ \n\n" + \
+\
+"...do pardon ͞th͞é ͏artifacts in the message. I tried my best to eliminate them this time, but...\n\n\n" + \
+\
+\
+"...oh,  ̛r̕ight̨,̕ ͝yo͝u͝ s.till need the gas mask to progress.\n" + \
+"Go back to the safe, it's between the ocean and the lookout point.\n\n" + \
+\
+"The code you need is "
+
 def safe_puzzle_write
   File.open(Oneshot::DOCS_PATH + "/DOCUMENT.oneshot.txt", 'w') do |file|
     file.puts(DOCUMENT_TEXT + $game_variables[20].to_s)
+  end
+end
+
+def safe_puzzle_postgame_write
+  File.open(Oneshot::DOCS_PATH + "/DOCUMENT.oneshot.txt", 'w') do |file|
+    file.puts(DOCUMENT_POSTGAME_TEXT + $game_variables[20].to_s)
   end
 end
