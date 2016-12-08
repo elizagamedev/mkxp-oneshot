2c5cc374cd11b593c544edcd27a8bccfa5ff7486
mshirt <mshirt@ualberta.ca>
2016-12-07 23:06:40 -0600

    Making game defullscreen during prompts

--- scripts/EdText.rb
+++ scripts/EdText.rb
@@ -3,12 +3,24 @@ module EdText
     Oneshot.msgbox Oneshot::Msg::YESNO, tr("Do you understand what this means?")
   end
   def self.info(text)
+    if(Graphics.fullscreen == true)
+	  Graphics.fullscreen = false
+	  $console = false
+	end
+	sleep 0.1
+	Graphics.update
     timestart = Time.now
     text.gsub!("\\p", $game_oneshot.player_name)
     Oneshot.msgbox Oneshot::Msg::INFO, tr(text)
     $game_temp.prompt_wait = ((Time.now - timestart) * 60).round
   end
   def self.yesno(text)
+    if(Graphics.fullscreen == true)
+	  Graphics.fullscreen = false
+	  $console = false
+	end
+	sleep 0.1
+	Graphics.update
     timestart = Time.now
     text.gsub!("\\p", $game_oneshot.player_name)
     result = Oneshot.msgbox Oneshot::Msg::YESNO, tr(text)
@@ -16,6 +28,12 @@ module EdText
     return result
   end
   def self.err(text)
+    if(Graphics.fullscreen == true)
+	  Graphics.fullscreen = false
+	  $console = false
+	end
+	sleep 0.1
+	Graphics.update
     timestart = Time.now
     text.gsub!("\\p", $game_oneshot.player_name)
     result = Oneshot.msgbox Oneshot::Msg::ERR, tr(text)
