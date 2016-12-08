ee0c895a1e36c3205b1d667f6e16d1c2aed7f340
mshirt <mshirt@ualberta.ca>
2016-12-08 02:19:46 -0600

    increasing delay for graphics update to catch up

--- scripts/EdText.rb
+++ scripts/EdText.rb
@@ -3,38 +3,38 @@ module EdText
     Oneshot.msgbox Oneshot::Msg::YESNO, tr("Do you understand what this means?")
   end
   def self.info(text)
+    timestart = Time.now
     if(Graphics.fullscreen == true)
 	  Graphics.fullscreen = false
 	  $console = false
 	end
-	sleep 0.1
+	sleep 0.2
 	Graphics.update
-    timestart = Time.now
     text.gsub!("\\p", $game_oneshot.player_name)
     Oneshot.msgbox Oneshot::Msg::INFO, tr(text)
     $game_temp.prompt_wait = ((Time.now - timestart) * 60).round
   end
   def self.yesno(text)
+    timestart = Time.now
     if(Graphics.fullscreen == true)
 	  Graphics.fullscreen = false
 	  $console = false
 	end
-	sleep 0.1
+	sleep 0.2
 	Graphics.update
-    timestart = Time.now
     text.gsub!("\\p", $game_oneshot.player_name)
     result = Oneshot.msgbox Oneshot::Msg::YESNO, tr(text)
     $game_temp.prompt_wait = ((Time.now - timestart) * 60).round
     return result
   end
   def self.err(text)
+    timestart = Time.now
     if(Graphics.fullscreen == true)
 	  Graphics.fullscreen = false
 	  $console = false
 	end
-	sleep 0.1
+	sleep 0.2
 	Graphics.update
-    timestart = Time.now
     text.gsub!("\\p", $game_oneshot.player_name)
     result = Oneshot.msgbox Oneshot::Msg::ERR, tr(text)
     $game_temp.prompt_wait = ((Time.now - timestart) * 60).round
