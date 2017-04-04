d4a22f2b816599af3e6726f7af358e1eebc6eeb6
Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2017-04-04 15:46:49 -0700

    Update make script
    Add progress messages and allow for user config of Steam game directory

--- make-oneshot-mac.sh
+++ make-oneshot-mac.sh
@@ -1,35 +1,59 @@
 #!/bin/sh
+set -e
 
-mac_version="0.3.2"
+# User-configurable variables
+mac_version="0.3.3"
+steam_game_dir=~/Library/Application\ Support/Steam/SteamApps/common/OneShot
+
+# Colors
+white="\033[0;37m"      # White - Regular
+bold="\033[1;37m"       # White - Bold
+cyan="\033[1;36m"       # Cyan - Bold
+green="\033[1;32m"      # Green - Bold
+color_reset="\033[0m"   # Reset Colors
+
+echo "${white}Compiling ${bold}SyngleChance v${mac_version} ${white}engine for macOS...${color_reset}\n"
 
 # Generate makefile and build main + journal
+echo "-> ${cyan}Generate makefile...${color_reset}"
 qmake MRIVERSION=2.3
+echo "-> ${cyan}Compile engine...${color_reset}"
 make -j4
+echo "-> ${cyan}Compile journal...${color_reset}"
 pyinstaller journal/mac/journal.spec --onefile --windowed
 
 # Create app bundles
+echo "-> ${cyan}Create app bundles...${color_reset}"
 mv OneShot.app OneShot_new.app
 cp -r patches/mac/OneShot_template.app ./OneShot.app
 cp OneShot_new.app/Contents/MacOS/OneShot OneShot.app/Contents/Resources/OneShot
 cp -r dist/_______.app _______.app
 
 # Set version number
+echo "-> ${cyan}Set version number...${color_reset}"
 rm OneShot.app/Contents/Info.plist
 rm _______.app/Contents/Info.plist
 m4 patches/mac/Info.plist.in -DONESHOTMACVERSION=$mac_version > OneShot.app/Contents/Info.plist
 m4 patches/mac/JournalInfo.plist.in -DONESHOTMACVERSION=$mac_version > _______.app/Contents/Info.plist
 
-# Compile scripts and install to Steam directory
-ruby rpgscript.rb ./scripts ~/Library/Application\ Support/Steam/SteamApps/common/OneShot/
-cp ~/Library/Application\ Support/Steam/SteamApps/common/OneShot/Data/xScripts.rxdata .
-rm -r ~/Library/Application\ Support/Steam/SteamApps/common/OneShot/OneShot.app
-rm -r ~/Library/Application\ Support/Steam/SteamApps/common/OneShot/_______.app
-cp -r OneShot.app ~/Library/Application\ Support/Steam/SteamApps/common/OneShot/OneShot.app
-cp -r _______.app ~/Library/Application\ Support/Steam/SteamApps/common/OneShot/_______.app
+# Compile scripts
+echo "-> ${cyan}Compile xScripts.rxdata...${color_reset}"
+ruby rpgscript.rb ./scripts "${steam_game_dir}"
+
+# Install to Steam directory
+echo "-> ${cyan}Install to Steam directory...${color_reset}"
+cp "${steam_game_dir}/Data/xScripts.rxdata" .
+rm -r "${steam_game_dir}/OneShot.app"
+rm -r "${steam_game_dir}/_______.app"
+cp -r OneShot.app "${steam_game_dir}/OneShot.app"
+cp -r _______.app "${steam_game_dir}/_______.app"
 
 # Cleanup
-make clean
+echo "-> ${cyan}Cleanup files...${color_reset}"
+# make clean
 rm -r OneShot_new.app
 rm -r journal/mac/__pycache__
 rm -r build
 rm -r dist
+
+echo "\n${green}Complete!  ${white}Please report any issues to https://github.com/vinyldarkscratch/mkxp-oneshot/issues${color_reset}"
