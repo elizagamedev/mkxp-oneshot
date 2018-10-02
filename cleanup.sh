8a5cbb4df09ebe2c8adcb7b6748e9a1c38131862
Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2018-10-02 14:23:57 -0700

    Update cleanup script and ignore Cmake junk

--- cleanup.sh
+++ cleanup.sh
@@ -7,4 +7,15 @@ rm -rf dist
 rm -rf _______.app
 rm -rf OneShot.app
 rm -rf OneShot
-make distclean
+rm -rf conanbuildinfo.cmake
+rm -rf CMakeFiles
+rm -rf cmake_install.cmake
+rm -rf CMakeCache.txt
+rm -rf conanbuildinfo.txt
+rm -rf conaninfo.txt
+if [ -f .qmake.stash ]
+then
+	make distclean
+else 
+	echo ""
+fi
