e815b6868e54107bf17778ad16b1da859f598538
Speak2Erase <54482069+Speak2Erase@users.noreply.github.com>
2020-06-25 18:26:25 -0700

    Update modshot-binding.cpp

--- binding-mri/modshot-binding.cpp
+++ binding-mri/modshot-binding.cpp
@@ -16,14 +16,14 @@ RB_METHOD(bounce_up)
 	int state;
 	for (int i = 0; i < 60; ++i) {
 		int max = 60 - i;
-		int y = 5;
+		int y = -5;
 		int x = 0;
 		SDL_SetWindowPosition(shState->rtData().window, absx + x, absy + y);
 		rb_eval_string_protect("sleep 0.02", &state);
 	}
 	for (int i = 0; i < 60; ++i) {
 		int max = 60 - i;
-		int y = -5;
+		int y = 5;
 		int x = 0;
 		SDL_SetWindowPosition(shState->rtData().window, absx + x, absy + y);
 		rb_eval_string_protect("sleep 0.02", &state);
@@ -31,17 +31,25 @@ RB_METHOD(bounce_up)
 	return Qnil;
 }
 
+RB_METHOD(modshotGetWindowPosition) {
+	int x, y;
+	SDL_GetWindowPosition(shState->rtData().window, &x, &y);
+	return rb_ary_new3(2, x, y);
+}
 
-
-
-
-
+RB_METHOD(modshotSetWindowPosition) {
+	int x, y;
+	rb_get_args(argc, argv, "ii", &x, &y);
+	SDL_SetWindowPosition(shState->rtData().window, x, y);
+	return Qnil;
+}
 
 void modshotBindingInit()
 {
 	VALUE module = rb_define_module("Modshot");
 	VALUE msg = rb_define_module_under(module, "Msg");
 	_rb_define_module_function(module, "bounce_up", bounce_up);
-
-
+	//_rb_define_module_function(module, "defhere", exposedname);
+	_rb_define_module_function(module, "modshotGetWindowPosition", getwin);
+	_rb_define_module_function(module, "modshotSetWindowPosition", setwin);
 }
