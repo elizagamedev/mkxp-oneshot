e16d51dc72b283385aa702a3186a593e448da94c
Speak2Erase <54482069+Speak2Erase@users.noreply.github.com>
2020-08-04 23:35:32 -0700

    start on gamejolt + discord api
    (leaving disc api until it's updated in october i think?)

---
+++ binding-mri/discord-binding.cpp
@@ -0,0 +1,9 @@
+#include "oneshot.h"
+#include "etc.h"
+#include "sharedstate.h"
+#include "binding-util.h"
+#include "binding-types.h"
+#include "eventthread.h"
+
+#include <SDL.h>
+#include <boost/crc.hpp>
--- binding-mri/gamejolt-binding.cpp
+++ binding-mri/gamejolt-binding.cpp
@@ -7,4 +7,17 @@
 
 #include <SDL.h>
 #include <boost/crc.hpp>
-#include 
+
+RB_METHOD(LoadUserData) {
+	char* path
+	rb_get_args(argc, argv, "z", &path);
+	return Qnil
+}
+
+void modshotgamejoltBindingInit()
+{
+	VALUE module = rb_define_module("Gamejolt");
+	VALUE msg = rb_define_module_under(module, "Msg");
+	_rb_define_module_function(Gamejolt, "LoadUserData", "LoadUserData)
+	//_rb_define_module_function(module, "defhere", exposedname);
+}
