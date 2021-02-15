c6837e4c750b3c1197bbaf1e3a8e07d056b8e755
CryroFox <44964856+CryroFox@users.noreply.github.com>
2021-02-15 14:20:19 -0800

    change the game to read from xscripts again lmao H

--- src/config.cpp
+++ src/config.cpp
@@ -182,7 +182,7 @@ void Config::read(int argc, char *argv[])
 	//Hardcode some ini/version settings
 	rgssVersion = 1;
 	game.title = "Aftermath";
-	game.scripts = "Data/Scripts.rxdata";
+	game.scripts = "Data/xScripts.rxdata";
 	defScreenW = 640;
 	defScreenH = 480;
 
