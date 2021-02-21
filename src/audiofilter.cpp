8ea6daae66079d99749ba688c01fb3cad4123d8e
rkevin <rk@rkevin.dev>
2021-02-21 15:58:47 -0800

    refactor interface (use signed 16bit), fixed rectifier

--- src/audiofilter.cpp
+++ src/audiofilter.cpp
@@ -6,6 +6,7 @@
 
 #include "audiofilter.h"
 #include "al-util.h"
+#include <util.h>
 #include <stdexcept>
 
 /* Base AudioFilter */
@@ -16,13 +17,13 @@ void AudioFilter::process(ALenum format, const ALvoid *data, ALsizei size, ALsiz
             process_mono((uint8_t*) data, size, freq);
             break;
         case AL_FORMAT_MONO16:
-            process_mono((uint16_t*) data, size, freq);
+            process_mono((int16_t*) data, size, freq);
             break;
         case AL_FORMAT_STEREO8:
             process_stereo((uint8_t*) data, size, freq);
             break;
         case AL_FORMAT_STEREO16:
-            process_stereo((uint16_t*) data, size, freq);
+            process_stereo((int16_t*) data, size, freq);
             break;
     }
 }
@@ -31,7 +32,7 @@ void AudioFilter::process_mono(uint8_t *data, int size, int freq) {
     throw std::logic_error("Not Implemented");
 }
 
-void AudioFilter::process_mono(uint16_t *data, int size, int freq) {
+void AudioFilter::process_mono(int16_t *data, int size, int freq) {
     throw std::logic_error("Not Implemented");
 }
 
@@ -39,46 +40,44 @@ void AudioFilter::process_stereo(uint8_t *data, int size, int freq) {
     throw std::logic_error("Not Implemented");
 }
 
-void AudioFilter::process_stereo(uint16_t *data, int size, int freq) {
+void AudioFilter::process_stereo(int16_t *data, int size, int freq) {
     throw std::logic_error("Not Implemented");
 }
 
 /* Rectifier filter */
 
-RectifierAudioFilter::RectifierAudioFilter(float probability){
-    threshold = (unsigned int) (RAND_MAX * probability);
-    if (threshold < 0)
-        threshold = RAND_MAX;
+RectifierAudioFilter::RectifierAudioFilter(float intensity) {
+    this->intensity = clamp(intensity, 0.0f, 1.0f);
 }
 
 void RectifierAudioFilter::process_mono(uint8_t *data, int size, int freq) {
     rectify(data, size / sizeof(uint8_t));
 }
 
-void RectifierAudioFilter::process_mono(uint16_t *data, int size, int freq) {
-    rectify(data, size / sizeof(uint16_t));
+void RectifierAudioFilter::process_mono(int16_t *data, int size, int freq) {
+    rectify(data, size / sizeof(int16_t));
 }
 
 void RectifierAudioFilter::process_stereo(uint8_t *data, int size, int freq) {
     rectify(data, size / sizeof(uint8_t));
 }
 
-void RectifierAudioFilter::process_stereo(uint16_t *data, int size, int freq) {
-    rectify(data, size / sizeof(uint16_t));
+void RectifierAudioFilter::process_stereo(int16_t *data, int size, int freq) {
+    rectify(data, size / sizeof(int16_t));
 }
 
 void RectifierAudioFilter::rectify(uint8_t *data, int count) {
     for (int i = 0; i < count; i++) {
-        if (data[i] & 0x80 && rand() < threshold) {
-            data[i] = ~data[i];
+        if (!(data[i] & 0x80)) {
+            data[i] = ((uint8_t) clamp((int) (-(int8_t) (data[i] ^ 0x80) * 2 * intensity) + (int8_t) (data[i] ^ 0x80), -0x80, 0x7f)) ^ 0x80;
         }
     }
 }
 
-void RectifierAudioFilter::rectify(uint16_t *data, int count) {
+void RectifierAudioFilter::rectify(int16_t *data, int count) {
     for (int i = 0; i < count; i++) {
-        if (data[i] & 0x8000 && rand() < threshold) {
-            data[i] = ~data[i];
+        if (data[i] < 0) {
+            data[i] = (int16_t) clamp((int) ((-(int)data[i]) * 2 * intensity) + data[i], -0x8000, 0x7fff);
         }
     }
 }
