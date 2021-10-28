c827494433106f31e058e8b9964f9d2bde29ff79
Speak2Erase <matthew@nowaffles.com>
2021-10-27 19:22:12 -0700

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -69,10 +69,8 @@ class MkxpConan(ConanFile):
     
     def build_requirements(self):
         if self.options.msys2:
-            #self.build_requires("msys2/cci.latest")
-            #self.build_requires("mingw-w64/8.1")
-            self.run("ls /home/", win_bash=True)
-            self.run("cat ~/.bashrc", win_bash=True)
+            self.build_requires("msys2/cci.latest")
+            self.build_requires("mingw-w64/8.1")
 
     def configure(self):
         if tools.os_info.is_windows:
