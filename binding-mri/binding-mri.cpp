96bd43cddfb300bf01eccf77b61dc9a9fd609810
Speak2Erase <matthew@nowaffles.com>
2021-11-04 22:34:42 -0700

    Update binding-mri.cpp

--- binding-mri/binding-mri.cpp
+++ binding-mri/binding-mri.cpp
@@ -621,8 +621,8 @@ static void mriBindingExecute()
 	// the three arguments are the executable name, and the '-e ""' is to tell ruby to run an empty file
 	// otherwise (since this parses options for the ruby executable) it's gonna wait on stdin for code
 	// --jit enables the jit i think
-	char options_argv1[] = "oneshot", options_argv2[] = "--jit", options_argv3[] = "--jit-verbose=1", options_argv4[] = "-e", options_argv5[] = "";
-	char* options_argv[] = {options_argv1, options_argv2, options_argv3, options_argv4, options_argv5, NULL};
+	char options_argv1[] = "oneshot", options_argv2[] = "--jit" options_argv3[] = "-e", options_argv4[] = "";
+	char* options_argv[] = {options_argv1, options_argv2, options_argv3, options_argv4, NULL};
 	ruby_options(5, options_argv);
 
 	rb_enc_set_default_external(rb_enc_from_encoding(rb_utf8_encoding()));
