8bfbe7d3a7125464b0d09396b2a9e169c9531f34
Speak2Erase <matthew@nowaffles.com>
2021-11-04 21:31:48 -0700

    Update binding-mri.cpp

--- binding-mri/binding-mri.cpp
+++ binding-mri/binding-mri.cpp
@@ -622,7 +622,7 @@ static void mriBindingExecute()
 	// otherwise (since this parses options for the ruby executable) it's gonna wait on stdin for code
 	// --jit enables the jit i think
 	char options_argv1[] = "oneshot", options_argv2[] = "-e", options_argv3[] = "--jit --jit-verbose=1";
-	char* options_argv[] = {options_argv1, options_argv2, options_argv3, NULL};
+	char* options_argv[] = {options_argv1, options_argv3, options_argv2, NULL};
 	ruby_options(3, options_argv);
 
 	rb_enc_set_default_external(rb_enc_from_encoding(rb_utf8_encoding()));
