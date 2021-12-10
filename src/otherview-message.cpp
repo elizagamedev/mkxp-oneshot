729cef125034ff055e0d2b27f8ff35aaccf5ea15
somedevfox <somedevfox@gmail.com>
2021-12-10 21:45:32 +0000

    OtherView ported to named pipes (much easier than zmq)

--- src/otherview-message.cpp
+++ src/otherview-message.cpp
@@ -1,87 +1,138 @@
+// Standard libraries.
+#include <vector>
+#include <stdexcept>
+#include <cstdlib>
+#include <iostream>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+// ModShot libraries.
 #include "otherview-message.h"
 #include "config.h"
 #include "debugwriter.h"
+#include "crash-handler.h"
 
-#include <zmq.hpp>
-#include <zmq_addon.hpp>
+// Boost Interprocess.
+#include <boost/asio.hpp>
+#include <boost/thread.hpp>
+#include <boost/process.hpp>
+
+#ifdef __linux
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#endif
+
+#include <stdio.h>
+
+
+using namespace boost::process;
+
+#define OSFM_NAMEDPIPE_NAME "OSFMOtherViewPipe_DontTouch"
+
+/*io_service ioService;
+
+void ovm_server_thread() {
+    tcp::acceptor acceptor(ioService, tcp::endpoint(tcp::v4(), OSFM_OTHERVIEW_PORT));
+
+    while(true) {
+        tcp::socket socket(ioService);
+        acceptor.accept(socket);
+
+        boost::asio::streambuf buf;
+        boost::system::error_code errorcode;
+        while(read(socket, buf, errorcode)) {
+            cout << &buf << endl;
+            if(errorcode) {
+                cout << "Error: " << errorcode << endl;
+                break;
+            }
+        }
+    }
+}
+
+void ovm_client_thread() {
+    tcp::socket socket(ioService);
+    tcp::endpoint endpoint(ip::address::from_string("127.0.0.1"), OSFM_OTHERVIEW_PORT);
+    socket.connect(endpoint);
+
+    boost::system::error_code errorcode;
+    boost::asio::streambuf buf;
+    socket.send("Echo!");
+    while(read(socket, buf, errorcode)) {
+        cout << &buf << endl;
+        if(errorcode) {
+            cout << "Error: " << errorcode << endl;
+        }
+    }
+}*/
+
+void ovh_write_named_pipe(string message) {
+    ofstream fifo(OSFM_NAMEDPIPE_NAME);
+
+    fifo << message << endl;
+    fifo.close();
+}
+
+void clearPipe() {
+    ofstream fifo(OSFM_NAMEDPIPE_NAME);
+    fifo.clear();
+    fifo.close();
+}
+
+string ovh_read_named_pipe() {
+    ifstream fifo(OSFM_NAMEDPIPE_NAME);
+
+    string str;
+    while(fifo >> str) {
+        str += str;
+    }
+    fifo.close();
+    clearPipe();
+
+    return str;
+}
 
 OtherViewMessager::OtherViewMessager(const Config &c):
     conf(c)
 {
-    //zmq::context_t ctx;
-    if (conf.isOtherView) { 
-        otherview = zmq::socket_t (ctx, ZMQ_PUSH);
-        mainwindow = zmq::socket_t (ctx, ZMQ_PULL);
-        otherview.bind("tcp://127.0.0.1:9698");
-        mainwindow.connect("tcp://127.0.0.1:9697");
-    } else {
-        otherview = zmq::socket_t (ctx, ZMQ_PULL);
-        mainwindow = zmq::socket_t (ctx, ZMQ_PUSH);
-        otherview.connect("tcp://127.0.0.1:9698");
-        mainwindow.bind("tcp://127.0.0.1:9697");
-    }
-}
+    Debug() << "Interprocessing start.";
+    try {
+        if (conf.isOtherView) { // Server-mode
+            ovh_write_named_pipe("Boo!");
+            Debug() << ovh_read_named_pipe();
 
-void OtherViewMessager::sendMsg(const char *str) {
-    size_t size = strlen(str); // Assuming your char* is NULL-terminated
-    zmq::message_t message(size);
-    std::memcpy (message.data(), str, size);
-
-    if (conf.isOtherView) {
-        otherview.send (message, zmq::send_flags::dontwait);
-    } else {
-        mainwindow.send (message, zmq::send_flags::dontwait);
-    }
-}
-
-const char* OtherViewMessager::getMsg() {
-    zmq::message_t message;
-    if (conf.isOtherView) {
-        auto res = mainwindow.recv (message, zmq::recv_flags::dontwait);
-
-        /*
-        Debug() << "message";
-        Debug() << message;
-        Debug() << res.has_value();
-        if (res.has_value()){
-            Debug() << res.value();
+            //OtherViewMessager::OpenOneShot();
+        } else {
+            //ovh_read_shared_memory();
+            //boost::thread c(ovm_client_thread);
         }
-        Debug() << "message data";
-        Debug() << message.data();
-        Debug() << sizeof(message.data());
-        */
-
-        if ((res.has_value() && (EAGAIN == res.value())) || zmq_errno() == EAGAIN) 
-            return "NO MESSAGES";
-
-        return static_cast<char*>(message.data());
-        
-    } else {
-        auto res = otherview.recv (message, zmq::recv_flags::dontwait);
-
-        /*
-        Debug() << message;
-        Debug() << res.has_value();
-        if (res.has_value()){
-            Debug() << res.value();
-        }
-        Debug() << message.data();
-        */
-        if ((res.has_value() && (EAGAIN == res.value())) || zmq_errno() == EAGAIN) 
-            return "NO MESSAGES";
-
-        return static_cast<char*>(message.data());
-
+    } catch(exception& e) {
+        std::cerr << "[EXCEPTION] " << e.what() << endl;
     }
-}
-// Ignore because 0MQ has some python zen ass bullshit and you can't check the amount of messages in a queue
-// I also can't figure out how to enable the active poller which would bypass this problem entirely
-int OtherViewMessager::unreadMsgs() {
-    return 0;
+    Debug() << "If you see this, interprocessing has succeeded.";
+    // LowLevelCrashHandler::upload("If you see this, this is not a crash, it's just a test.\n   -s");
 }
 
-void OtherViewMessager::closeSockets() {
-    otherview.close();
-    mainwindow.close();
-    ctx.close();
+void openoneshot_thread() {
+    ipstream pipeStream;
+    child c("./lib/oneshot", std_out > pipeStream);
+
+    string line;
+    while(pipeStream && getline(pipeStream, line) && !line.empty())
+        cerr << "[Child ModShot] " << line << endl;
+}
+
+void OtherViewMessager::OpenOneShot() {
+    boost::thread childModShot(openoneshot_thread);
+}
+
+void OtherViewMessager::sendMsg(string str) {
+    ovh_write_named_pipe(str);
+}
+
+string OtherViewMessager::getMsg() {
+    if(ovh_read_named_pipe() != "")
+        return ovh_read_named_pipe();
+    else return nullptr;
 }
